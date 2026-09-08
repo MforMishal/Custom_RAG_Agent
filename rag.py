a05cc7d27c703a1c57e93e3cab3e3875814e700e
Mishal Munir <mishalmunir70@gmail.com>
2026-09-08 09:14:57 +0500

    Fix import casing for RunnablePassthrough

--- rag.py
+++ rag.py
@@ -10,7 +10,7 @@ from langchain_core.output_parsers import StrOutputParser
 
 from langchain_core.prompts import ChatPromptTemplate
 
-from langchain_core.runnables import RunnablePassThrough
+from langchain_core.runnables import RunnablePassthrough
 
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
